fae010e615725cca861004b3eae775b946dbbf81
Veselin Veselinov <veselin0034@gmail.com>
2022-11-13 02:54:20 +0200

    Update 06.ReplaceRepeatingChars

--- TextProcessing-Exercise/06.ReplaceRepeatingChars/Program.cs
+++ TextProcessing-Exercise/06.ReplaceRepeatingChars/Program.cs
@@ -8,24 +8,20 @@ namespace _06.ReplaceRepeatingChars
         {
             // step 1 = Reading the input from the console
             string text = Console.ReadLine();
-
             // Step 2 => text = aaaabbbccc => previousCahr = text[0]
-            char previousChar = text[0];  //'a'
-            Console.WriteLine(previousChar);
+            char previousChar = text[0];   // 'a'
+            Console.Write(previousChar);
             // step 3 iterate through the text starting 1 index
             for (int i = 1; i < text.Length; i++)
             {
-                // Step 4 Take the currentChar and make check with the previous
+                // Step 4 taking the currentChar and making check with the previous
                 char currentChar = text[i];
-                if (previousChar != currentChar) // a != b then previousChar = b
+                if (previousChar != currentChar) // a != b then previusChar = b
                 {
                     previousChar = currentChar;
-                    Console.Write(previousChar);  //'ab'
+                    Console.Write(previousChar); // 'ab'
                 }
-
             }
-
-
         }
     }
 }
