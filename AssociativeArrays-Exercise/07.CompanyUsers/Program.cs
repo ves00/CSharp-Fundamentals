05449cc1c488f46ed683573b7a2bf86266c3a4d5
Veselin Veselinov <veselin0034@gmail.com>
2022-11-16 23:28:36 +0200

    Update

---
+++ AssociativeArrays-Exercise/07.CompanyUsers/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.CompanyUsers
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var employees = new Dictionary<string, List<string>>();
+
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] inputArgs = input.Split(" -> ");
+
+                string company = inputArgs[0];
+                string employeeId = inputArgs[1];
+
+                if (!employees.ContainsKey(company))
+                {
+                    employees.Add(company, new List<string>());
+                    employees[company].Add(employeeId);
+                }
+                else
+                {
+                    if (!employees[company].Contains(employeeId))
+                    {
+                        employees[company].Add(employeeId);
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var company in employees)
+            {
+                Console.WriteLine($"{company.Key}");
+
+                foreach (var employee in company.Value)
+                {
+                    Console.WriteLine($"-- {employee}");
+                }
+            }
+        }
+    }
+}
