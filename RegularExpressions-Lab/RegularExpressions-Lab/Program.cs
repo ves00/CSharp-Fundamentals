059fc1ff08967436e3bd6fca335941321b2dc1d8
Veselin Veselinov <veselin0034@gmail.com>
2022-11-16 22:50:39 +0200

    Update Program.cs

--- RegularExpressions-Lab/RegularExpressions-Lab/Program.cs
+++ RegularExpressions-Lab/RegularExpressions-Lab/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace RegularExpressions_Lab
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            string pattern = @"(\w+)";
+            string input = "Vesko333###@90awd";
+            Regex regex = new Regex(pattern);
+            //bool isMatch = regex.isMatch(input);
+            Match match = regex.Match(input);
+            Console.WriteLine(match.Groups[1]);
         }
     }
 }
